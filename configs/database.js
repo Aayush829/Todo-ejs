667535005fd8fdfe2f9c9f7b9162ddd48431b083
Aayush Pandey <aayushpandey829@gmail.com>
2021-02-24 16:48:12 +0530

    first commit

---
+++ configs/database.js
@@ -0,0 +1,7 @@
+module.exports.config = {
+    uri: 'mongodb+srv://pandey:aayu829@cluster0.n0hp2.mongodb.net/todos?retryWrites=true&w=majority',
+    options: {
+        dbName: 'todo-app',
+        useNewUrlParser: true
+    },
+}
